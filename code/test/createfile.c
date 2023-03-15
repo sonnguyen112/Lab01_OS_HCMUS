0561d6461ec18c5e65a74e6b4ef3b73e1555be0f
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2023-03-15 09:13:14 +0000

    printString

--- code/test/createfile.c
+++ code/test/createfile.c
@@ -3,8 +3,25 @@
 #define maxlen 32
 int main(int argc, char* argv[])
 {
-    int len;
-    char filename[maxlen + 1];
+    // int stdin;
+    // char fileName[maxlen + 1];
+    // stdin = Open("stdin", 2);
+    // if (stdin != -1)
+	// {
+	// 	// int len = Read(fileName, MAX_LENGTH, stdin); // Goi ham Read doc ten file vua nhap
+    //     int len = 5;
+		
+	// 	if(len <= 1)
+	// 		PrintString(" -> Ten file khong hop le!!!\n\n");
+	// 	else
+	// 	{
+	// 		if (CreateFile(fileName) == 0) // Goi ham CreateFile
+	// 		{
+	// 			PrintString(" -> Create file thanh cong.\n\n");
+	// 		}
+	// 	}
+	// 	Close(stdin); // Goi ham Close de dong stdin
+	// }
     /*Create a file*/
     if (Create("text.txt") == -1)
     {
