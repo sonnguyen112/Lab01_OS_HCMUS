cd1f085e292de6aa7cbbac8547cc270c2441b4df
khoicool <khoicool@khoiserver.com>
2023-03-15 03:14:08 +0000

    khdff

--- code/test/createfile.c
+++ code/test/createfile.c
@@ -3,22 +3,22 @@
 #define maxlen 32
 int main()
 {
-    int len;
-    char filename[maxlen + 1];
-    /*Create a file*/
-    if (Create("text.txt") == -1)
-    {
-        // xuất thông báo lỗi tạo tập tin
-        // print("\nCreate file ~");
-        // print(filename);
-        // print(" fail.");
-    }
-    else
-    {
-        // xuất thông báo tạo tập tin thành công
-        // print("\nCreate file ~");
-        // print(filename);
-        // print(" success.~"); 
-    }
+    // // int len;
+    // // char filename[maxlen + 1];
+    // /*Create a file*/
+    // if ( == -1)
+    // {
+    //     // xuất thông báo lỗi tạo tập tin
+    //     // print("\nCreate file ~");
+    //     // print(filename);
+    //     // print(" fail.");
+    // }
+    // else
+    // {
+    //     // xuất thông báo tạo tập tin thành công
+    //     // print("\nCreate file ~");
+    //     // print(filename);
+    //     // print(" success.~"); 
+    // }
     Halt(); 
 }
--- code/test/openFileID.c
+++ code/test/openFileID.c
@@ -3,17 +3,17 @@
 
 int main()
 {
-	OpenFileId fileId;
-	int filesz, i;
-	char c;
-	char name[255];
-	// PrintString("Input file name:");
-	// ReadString(name, 255);
-	if ( (fileId= Open(name)) == -1)
-	{
-		// PrintString("Can not open file ");
-		// return 0;
-	}
+	// OpenFileId fileId;
+	// int filesz, i;
+	// char c;
+	// char name[255];
+	// // PrintString("Input file name:");
+	// // ReadString(name, 255);
+	// if ( (fileId= Open(name)) == -1)
+	// {
+	// 	// PrintString("Can not open file ");
+	// 	// return 0;
+	// }
 	// filesz = SeekFile(-1, fileId);
 	// i = 0;
 	// SeekFile(0, fileId);
@@ -23,6 +23,24 @@ int main()
 	// 	// Read(&c, 1, fileId);
 	// 	// PrintChar(c);
 	// } 
-	Close(fileId);
-	Halt();
+	// Close(fileId);
+	// Halt();
+	int len;
+    char filename[maxlen + 1];
+    /*Create a file*/
+    if (Create("text.txt") == -1)
+    {
+        // xuất thông báo lỗi tạo tập tin
+        // print("\nCreate file ~");
+        // print(filename);
+        // print(" fail.");
+    }
+    else
+    {
+        // xuất thông báo tạo tập tin thành công
+        // print("\nCreate file ~");
+        // print(filename);
+        // print(" success.~"); 
+    }
+    Halt(); 
 }
