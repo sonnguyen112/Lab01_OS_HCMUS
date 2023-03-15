9a731974cc4215b35d508be3e3aaccb036286841
khoicool <khoicool>
2023-03-15 07:09:25 +0000

    khoi

--- code/test/createfile.c
+++ code/test/createfile.c
@@ -3,7 +3,6 @@
 #define maxlen 32
 int main(int argc, char* argv[])
 {
-<<<<<<< HEAD
     int len;
     char filename[maxlen + 1];
     /*Create a file*/
@@ -16,24 +15,4 @@ int main(int argc, char* argv[])
         PrintString("Create file successfully");
     }
     Halt();
-=======
-    // // int len;
-    // // char filename[maxlen + 1];
-    // /*Create a file*/
-    // if ( == -1)
-    // {
-    //     // xuất thông báo lỗi tạo tập tin
-    //     // print("\nCreate file ~");
-    //     // print(filename);
-    //     // print(" fail.");
-    // }
-    // else
-    // {
-    //     // xuất thông báo tạo tập tin thành công
-    //     // print("\nCreate file ~");
-    //     // print(filename);
-    //     // print(" success.~"); 
-    // }
-    Halt(); 
->>>>>>> cd1f085e292de6aa7cbbac8547cc270c2441b4df
 }
