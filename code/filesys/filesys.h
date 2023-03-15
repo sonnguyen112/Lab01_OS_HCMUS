b26f40387c15807ccd931615df9d9598f0b942de
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2023-03-15 09:20:32 +0000

    aaa

--- code/filesys/filesys.h
+++ code/filesys/filesys.h
@@ -89,7 +89,7 @@ class FileSystem {
 					// the disk, so initialize the directory
     					// and the bitmap of free blocks.
 
-    bool Create(char *name, int initialSize);  	
+    bool Create(char *name);  	
 					// Create a file (UNIX creat)
 
     OpenFile* Open(char *name); 	// Open a file (UNIX open)
