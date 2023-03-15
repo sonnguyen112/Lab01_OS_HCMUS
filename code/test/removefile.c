62b829f9c6253b05182a8501f54773b7466279a8
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2023-03-15 03:30:40 +0000

    commit

--- code/test/removefile.c
+++ code/test/removefile.c
@@ -1,18 +1,18 @@
 #include "syscall.h"
 #include "copyright.h"
 #define maxlen 32
-int main(int argc, char* argv[])
+int main()
 {
     int len;
     char filename[maxlen + 1];
     /*Create a file*/
-    if (Create("text.txt") == -1)
+    if (Remove("text.txt") == -1)
     {
         // xuất thông báo lỗi tạo tập tin
     }
     else
     {
-        PrintString("Create file successfully");
+        // xuất thông báo tạo tập tin thành công
     }
     Halt();
 }
